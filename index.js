0079481ad0fbf1f6864074d2ff66a6c3bb603e8c
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-27 17:48:19 +0600

    delete api

--- index.js
+++ index.js
@@ -47,7 +47,14 @@ async function run() {
             res.send(result)
         })
 
-        
+        //11 delete 
+        app.delete('/service/:id' , async(req,res) => {
+            const id = req.params.id 
+            const query = {_id:ObjectId(id)}
+            const result = await serviceCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
     } 
     finally {
